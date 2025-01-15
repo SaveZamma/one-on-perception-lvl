7ca5ca99995d624b2e8f31201294214585696922
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2025-01-15 16:25:21 +0100

    Create new views

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>One On Perception</title>
+
+        <!-- Fonts -->
+        <!-- <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" /> -->
+
+        <!-- Styles -->
+        {{-- <link href="../resources/css/app.css" rel="stylesheet" type="text/css"/> --}}
+    </head>
+    <body>
+        <section class="presentaion">
+            <h1>One On Perception</h1>
+            <p>Find everything you need for your adventures.</p>
+            <p>Be it dice, miniatures, maps or even an owlbear's egg, we have it all.</p>
+            <p>Explore our catalog today and start building your next adventure.</p>
+        </section>
+
+    </body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>One On Perception - Login</title>
+
+        <!-- Fonts -->
+
+        <!-- Styles -->
+        <!-- <link href="../css/app.css" rel="stylesheet" type="text/css"/> -->
+    </head>
+    <body>
+        <header>
+            <h1>One On Perception</h1>
+        </header>
+
+    </body>
+</html>
