94482a3e23fa734e70d37f0ef9c96a7ccc79839f
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2025-02-09 17:32:15 +0100

    Update navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,10 +5,24 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>One On Perception</title>
         @vite('resources/css/app.css')
+        <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
     </head> 
     <body>
         <nav class="top-navbar">
-            <h1 class="primary" style="">One On Perception</h1>
+            <div class="nav-section">
+                <svg class="primary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" alt="menu">
+                 <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/>
+                </svg>
+                <h1 class="primary" style="">One On Perception</h1>
+            </div>
+            <div class="nav-section">
+                <svg class="primary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" alt="shopping-bag">
+                    <path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64l0 48-128 0 0-48zm-48 48l-64 0c-26.5 0-48 21.5-48 48L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-208c0-26.5-21.5-48-48-48l-64 0 0-48C336 50.1 285.9 0 224 0S112 50.1 112 112l0 48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/>
+                </svg>
+                <svg class="secondary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" alt="user">
+                    <path d="M399 384.2C376.9 345.8 335.4 320 288 320l-64 0c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/>
+                </svg>
+            </div>
             {{-- <a href="{{ route('marketplace.index')}}">Marketplace</a> --}}
         </nav>
 
