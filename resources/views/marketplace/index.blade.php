0838c8606fc802dc1b32b45211229958465ad2d1
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2025-02-09 15:34:54 +0100

    Add banner div

--- resources/views/marketplace/index.blade.php
+++ resources/views/marketplace/index.blade.php
@@ -1,31 +1,36 @@
 <x-layout>
     <h2>Welcome to our marketplace</h2>
+
+
+
     <a href="marketplace/my">My shop</a>
+    <section class="promotion">
+        <div class="banner" style="background-image: url(https://picsum.photos/800); background-size: cover;">
+
+        </div>
+    </section>
+    
     <section class="dashboard">
+        
+        <input type="text" placeholder="Search for products"/>
+
         <ul class="products-list">
-            @foreach ($products as $product)
-                <li class="product card"
-                    style="background-image: url(https://picsum.photos/800); background-size: cover;"
-                    {{-- style="background-image: url({{ $product["image"] }})" --}}
-                >
-                <div class="card-title-container fx-col">
-                    <h3>{{ $product["name"] }}</h3>
-                    <div class="fx-row fx-space-between">
-                        <p>€ {{ $product["price"] }}</p>
-                        <a href="{{ route('marketplace.product', ['id' => $product['id']]) }}">View</a>
-                    </div>
+        @foreach ($products as $product)
+            <li class="product card"
+                style="background-image: url(https://picsum.photos/800); background-size: cover;"
+                {{-- style="background-image: url({{ $product["image"] }})" --}}
+            >
+            <div class="card-title-container fx-col">
+                <h3>{{ $product["name"] }}</h3>
+                <div class="fx-row fx-space-between">
+                    <p>€ {{ $product["price"] }}</p>
+                    <a href="{{ route('marketplace.product', ['id' => $product['id']]) }}">View</a>
                 </div>
-                    
-                    {{-- <x-card href="{{ route('marketplace.product', ['id' => $product['id']]) }}">
-                        <div style="background-image: url(https://picsum.photos/800)">
-                            <h3>{{ $product["name"] }}</h3>
-                            <p>{{ $product["price"] }}</p>
-                        </div>
-                    </x-card> --}}
-                </li>
-            @endforeach
+            </div>
+            </li>
+        @endforeach
         </ul>
 
-        {{ $products->links() }}
+        {{-- {{ $products->links() }} --}}
     </section>
 </x-layout>
