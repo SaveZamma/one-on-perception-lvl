ca201dadc2e9973d50c65b977017afd345ee0b35
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2025-02-12 17:41:48 +0100

    Implement logout

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,2 +1,9 @@
-<h1>profile page is working!</h1>
-<p>Profile page is working fine</p>
+<x-layout>
+    <h1>profile page is working!</h1>
+    <p>Profile page is working fine</p>
+
+    <form action="{{ route('logout') }}" method="POST">
+        @csrf
+        <button type="submit">Logout</button>
+    </form>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,12 @@ $isLogged = false;
 
 Route::get('/', function () {
     return view('landing');
-});
+})->name('landing');
 Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
 Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/profile', [ProfileController::class, 'profile'])->name('profile');
 
