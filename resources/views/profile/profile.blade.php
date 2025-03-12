5009da8f065442383c22e5f7bc34e8d9d9a72c3a
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2025-03-12 18:57:33 +0100

    Update profile page

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -1,32 +1,44 @@
 <x-layout>
     @vite('resources/css/profile.css')
 
-    
-    <section class="profile-section-card">
-        <h2>{{ Auth::user()->username }}</h2>
-    
-        {{-- <p>{{ Auth::user()->email}}</p> --}}
-        <p>Member since: {{ Auth::user()->created_at->format('d M Y') }}</p>
-
-    </section>
-
-    <section class="profile-actions">
-
-        <div class="profile-section-card with-shadow-sm">
-            <h2>My Orders</h2>
-        </div>
+    <div class="profile-page-container">    
+        <section class="profile-section-card">
+            <header class="fx-col">
+                <img class="profile-pic" src="{{ Auth::user()->avatar }}" alt="Profile Picture">
+                <h2>{{ Auth::user()->username }}</h2>
+            </header>
         
-        <div class="profile-section-card with-shadow-sm">
-            <h2>My Payments</h2>
-        </div>
-        
-        <div class="profile-section-card with-shadow-sm">
-            <h2>Profile Details</h2>
-        </div>
-    </section>
+            <p>Member since: {{ Auth::user()->created_at->format('d M Y') }}</p>
+            <p>From: {{ Auth::user()->country }}</p>
+        </section>
 
-    <form action="{{ route('logout') }}" method="POST">
-        @csrf
-        <button type="submit" class="glass fill-width with-shadow-sm">Logout</button>
-    </form>
+        <section class="profile-actions">
+            <div class="profile-action-btn with-shadow-sm">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" alt="boxes icon">
+                    <path d="M256 48c0-26.5 21.5-48 48-48L592 0c26.5 0 48 21.5 48 48l0 416c0 26.5-21.5 48-48 48l-210.7 0c1.8-5 2.7-10.4 2.7-16l0-242.7c18.6-6.6 32-24.4 32-45.3l0-32c0-26.5-21.5-48-48-48l-112 0 0-80zM571.3 347.3c6.2-6.2 6.2-16.4 0-22.6l-64-64c-6.2-6.2-16.4-6.2-22.6 0l-64 64c-6.2 6.2-6.2 16.4 0 22.6s16.4 6.2 22.6 0L480 310.6 480 432c0 8.8 7.2 16 16 16s16-7.2 16-16l0-121.4 36.7 36.7c6.2 6.2 16.4 6.2 22.6 0zM0 176c0-8.8 7.2-16 16-16l352 0c8.8 0 16 7.2 16 16l0 32c0 8.8-7.2 16-16 16L16 224c-8.8 0-16-7.2-16-16l0-32zm352 80l0 224c0 17.7-14.3 32-32 32L64 512c-17.7 0-32-14.3-32-32l0-224 320 0zM144 320c-8.8 0-16 7.2-16 16s7.2 16 16 16l96 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-96 0z"/>
+                </svg>
+                <p>My Orders</p>
+            </div>
+            <div class="profile-action-btn with-shadow-sm">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" alt="money bill icon">
+                    <path d="M0 112.5L0 422.3c0 18 10.1 35 27 41.3c87 32.5 174 10.3 261-11.9c79.8-20.3 159.6-40.7 239.3-18.9c23 6.3 48.7-9.5 48.7-33.4l0-309.9c0-18-10.1-35-27-41.3C462 15.9 375 38.1 288 60.3C208.2 80.6 128.4 100.9 48.7 79.1C25.6 72.8 0 88.6 0 112.5zM288 352c-44.2 0-80-43-80-96s35.8-96 80-96s80 43 80 96s-35.8 96-80 96zM64 352c35.3 0 64 28.7 64 64l-64 0 0-64zm64-208c0 35.3-28.7 64-64 64l0-64 64 0zM512 304l0 64-64 0c0-35.3 28.7-64 64-64zM448 96l64 0 0 64c-35.3 0-64-28.7-64-64z"/>
+                </svg>
+                <p>My Payments</p>
+            </div>
+            <div class="profile-action-btn with-shadow-sm">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" alt="user details icon">
+                    <path d="M224 0a128 128 0 1 1 0 256A128 128 0 1 1 224 0zM178.3 304l91.4 0c11.8 0 23.4 1.2 34.5 3.3c-2.1 18.5 7.4 35.6 21.8 44.8c-16.6 10.6-26.7 31.6-20 53.3c4 12.9 9.4 25.5 16.4 37.6s15.2 23.1 24.4 33c15.7 16.9 39.6 18.4 57.2 8.7l0 .9c0 9.2 2.7 18.5 7.9 26.3L29.7 512C13.3 512 0 498.7 0 482.3C0 383.8 79.8 304 178.3 304zM436 218.2c0-7 4.5-13.3 11.3-14.8c10.5-2.4 21.5-3.7 32.7-3.7s22.2 1.3 32.7 3.7c6.8 1.5 11.3 7.8 11.3 14.8l0 30.6c7.9 3.4 15.4 7.7 22.3 12.8l24.9-14.3c6.1-3.5 13.7-2.7 18.5 2.4c7.6 8.1 14.3 17.2 20.1 27.2s10.3 20.4 13.5 31c2.1 6.7-1.1 13.7-7.2 17.2l-25 14.4c.4 4 .7 8.1 .7 12.3s-.2 8.2-.7 12.3l25 14.4c6.1 3.5 9.2 10.5 7.2 17.2c-3.3 10.6-7.8 21-13.5 31s-12.5 19.1-20.1 27.2c-4.8 5.1-12.5 5.9-18.5 2.4l-24.9-14.3c-6.9 5.1-14.3 9.4-22.3 12.8l0 30.6c0 7-4.5 13.3-11.3 14.8c-10.5 2.4-21.5 3.7-32.7 3.7s-22.2-1.3-32.7-3.7c-6.8-1.5-11.3-7.8-11.3-14.8l0-30.5c-8-3.4-15.6-7.7-22.5-12.9l-24.7 14.3c-6.1 3.5-13.7 2.7-18.5-2.4c-7.6-8.1-14.3-17.2-20.1-27.2s-10.3-20.4-13.5-31c-2.1-6.7 1.1-13.7 7.2-17.2l24.8-14.3c-.4-4.1-.7-8.2-.7-12.4s.2-8.3 .7-12.4L343.8 325c-6.1-3.5-9.2-10.5-7.2-17.2c3.3-10.6 7.7-21 13.5-31s12.5-19.1 20.1-27.2c4.8-5.1 12.4-5.9 18.5-2.4l24.8 14.3c6.9-5.1 14.5-9.4 22.5-12.9l0-30.5zm92.1 133.5a48.1 48.1 0 1 0 -96.1 0 48.1 48.1 0 1 0 96.1 0z"/>
+                </svg>
+                <p>Profile Details</p>
+            </div>
+        </section>
+
+        <form action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button type="submit" class="glass fill-width with-shadow-sm logout-btn">
+                Log out
+            </button>
+        </form>
+        
+    </div>
 </x-layout>
