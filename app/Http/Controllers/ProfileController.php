35ac5d4cfcc651328d360913c478b57341784054
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2025-04-23 12:27:30 +0200

    Update wishlist page

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use App\Models\Transaction;
+use App\Models\Wishlist;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
@@ -25,8 +27,14 @@ class ProfileController extends Controller
 
     
     public function myWishlist() {
-        // $orders = Order::where('user_id', Auth::id())->pluck('id');
-        // $trans = Transaction::whereIn('order_id', $orders)->get();
-        return view('profile/wishlist');//, ["transactions" => $trans]);
+        $products = $products = DB::table('wishlist_products')
+            ->join('products', 'wishlist_products.product_id', '=', 'products.id')
+            ->where('wishlist_products.user_id', Auth::id())
+            ->select('products.*', 'wishlist_products.wishlist_id')
+            ->get();
+        $wishlistIDs = $products->pluck('wishlist_id');
+        $wishlists = Wishlist::whereIn('id', $wishlistIDs)->get();
+    
+        return view('profile/wishlist', ["wishlists" => $wishlists, "products" => $products]);
     }
 }
--- resources/views/profile/wishlist.blade.php
+++ resources/views/profile/wishlist.blade.php
@@ -2,4 +2,33 @@
 
 @section('page-content')
     <h1>My Wishlist</h1>
+
+    <ul class="wishlist-list">
+        @if ($wishlists->count() == 0)
+            <p>You don't have any wishlists.</p>
+        @else
+            @foreach ($wishlists as $wishlist)
+                <li class="card pad-2">
+                    <h2>{{ $wishlist['name'] }}</h2>
+                    <p>{{ $wishlist['description'] }}</p>
+
+                    <ul class="product-list">
+                        @foreach ($products as $product)
+                            @if ($product->wishlist_id == $wishlist['id'])
+                                <li class="card pad-2">
+                                    {{-- <img src="{{$product->imagePath}}" alt="{{$product->name}}"> --}}
+                                    {{-- <p class="price-tag"><strong>{{ $product->price }}</strong></p> --}}
+
+                                    <a href="{{ route('marketplace.product', ['id' => $product->id]) }}">
+                                        {{ $product->name }}
+                                    </a>
+                                </li>
+                            @endif
+                        @endforeach
+                    </ul>
+
+                </li>
+            @endforeach    
+        @endif
+    </ul>
 @endsection
