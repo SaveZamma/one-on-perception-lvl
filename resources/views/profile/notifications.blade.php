ccbc810a1995c7ffb7e070d26b0eb25797450954
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2025-05-24 18:47:29 +0200

    Update notification css

--- resources/views/profile/notifications.blade.php
+++ resources/views/profile/notifications.blade.php
@@ -11,7 +11,8 @@
         @foreach ($notifications as $n)
             <li class="card-border fx-row fx-space-between v-align-center" style="padding: 0.2rem 1rem;">
                 <div class="notification fx-col">
-                    <h3>{{$n['created_at']->format('d M Y H:i')}} - {{$n['title']}}</h3>
+                    <span class="infotext">{{$n['created_at']->format('d M Y H:i')}}</span>
+                    <h3>{{$n['title']}}</h3>
                     <p>{{$n['text']}}</p>
                 </div>
                 <div class="fx-row">
