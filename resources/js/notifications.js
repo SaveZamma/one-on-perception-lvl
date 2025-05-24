d79efefc5b6a184716f04e8fa376c8965719fac7
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2025-05-24 16:11:50 +0200

    Delete notifications

--- resources/js/notifications.js
+++ resources/js/notifications.js
@@ -2,11 +2,42 @@ const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
 
 let deleteBtns = document.querySelectorAll('button.delete-btn');
 
-for (let btn of deleteBtns) {
-    btn.addEventListener('click', function (e) {
-        const notifId = button.dataset.notifId;
 
-        let url = this.getAttribute('href');
+function mapToJsonOrNull(response) {
+    return response.ok 
+        ? response.json().then(data => ({ data: data, status: response.status })) 
+        : null;
+}
+
+function deleteNotification(btn) {
+    const route = '/notification/delete';
+    const notifId = btn.dataset.notifId;
+    fetch(route, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': csrfToken,
+        },
+        body: JSON.stringify({ notification_id: notifId })
+    })
+    .then(response => mapToJsonOrNull(response))
+    .then(data => {
+        if (data === null) {
+            throw new Error('Failed to toggle wishlist');
+        } else {
+            Toastify({
+                text: "Notification deleted",
+                duration: 2000,
+                style: {
+                    background: "linear-gradient(to right, #29A3A3, #FE7171)",
+                },
+            }).showToast();
+            btn.parentElement.parentElement.remove()
+        }
     });
+}
+
+for (let btn of deleteBtns) {
+    btn.addEventListener('click', function (e) { deleteNotification(btn) });
 }
 
